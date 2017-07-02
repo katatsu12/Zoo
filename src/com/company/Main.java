6a758ea0ddc32a3e777e53c3a93c5313470486ab
Den Zubritskiy <katatsu12@gmail.com>
2017-07-03 02:43:25 +0300

    update zoo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,17 @@ public class Main {
         Animals[] array = new Animals[] {new Giraffe(), new Lion(), new Monkey()};
         for (int x = 0; x < array.length; x++)
             array[x].Beast();
+
+        Lion lion = new Lion();
+        Giraffe giraffe = new Giraffe();
+        Monkey monkey = new Monkey();
+
+        say(lion);
+        say(giraffe);
+        say(monkey);
+    }
+
+    public static void say(Animals animal){
+        animal.Beast();
     }
 }
